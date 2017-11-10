38e6eea3ade1e8e678b45f2444ce2ebbd59bdc73
J.O.Price <notantisocial@users.noreply.github.com>
2017-11-09 20:26:53 -0500

    Moved Mine Controller into Game instance and set up some test suites

--- src/GameInstance.java
+++ src/GameInstance.java
@@ -44,4 +44,59 @@ public class GameInstance {
 			System.out.println();
 		}
 	}
+
+	public void processInput(int x, int y, boolean flag, int sideL) {
+		if (gameMineField[x][y].getViewStatus() == ViewStatus.COVERED) {
+			if (flag) {
+				gameMineField[x][y].setViewState(ViewStatus.FLAGGED);
+			} else {
+				InitCellState currentInitState = gameMineField[x][y].getCellState();
+				if (currentInitState == InitCellState.Mine) {
+					// you lose - please add a you lose method
+				} else {
+					// Recursive method for uncovering
+					sweepAndCheck(x, y, sideL);
+				}
+			}
+		} else {
+			// Don't update board
+		}
+
+	}
+
+	public void sweepAndCheck(int x, int y, int sideL) {
+		if (gameMineField[x][y].getCellState() == InitCellState.Free) {
+			// Nested/repeated if so we don't run check neighbors for uncovered free cells
+			if (gameMineField[x][y].getViewStatus() == ViewStatus.COVERED) {
+				sweepNear(x, y, sideL);
+			}
+		} else {
+			// Case for numbers
+			gameMineField[x][y].setViewState(ViewStatus.UNCOVERED);
+		}
+	}
+
+	public void sweepNear(int x, int y, int sideL) {
+		// check cells in all directions, increment for each mine found starting
+		sweepAt(y - 1, x - 1, sideL); // NW
+		sweepAt(y - 1, x, sideL); // N
+		sweepAt(y - 1, x + 1, sideL); // NE
+		sweepAt(y, x - 1, sideL); // W
+		sweepAt(y, x + 1, sideL); // E
+		sweepAt(y + 1, x - 1, sideL); // SW
+		sweepAt(y + 1, x, sideL); // S
+		sweepAt(y + 1, x + 1, sideL); // SE
+
+	}
+
+	public void sweepAt(int x, int y, int sideL) {
+		// we need to check also that we're not out of array bounds as that would
+		// be an error
+		if (y >= 0 && y < sideL && x >= 0 && x < sideL && (gameMineField[x][y].getViewStatus() == ViewStatus.COVERED)) {
+			gameMineField[x][y].setViewState(ViewStatus.UNCOVERED);
+			if (gameMineField[y][x].getCellState() == InitCellState.Free) {
+				sweepAndCheck(x, y, sideL);
+			}
+		}
+	}
 }
--- src/MineSweeperMain.java
+++ src/MineSweeperMain.java
@@ -1,41 +1,29 @@
+import java.util.Scanner;
+
 public class MineSweeperMain {
 
 	public static void main(String[] args) {
-
-		InitCellState[][] trueMF = InitMinefield.genMineFieldEnums(10, 10);
-
-		for (InitCellState[] row : trueMF) {
-			for (int i = 0; i < row.length; i++) {
-				System.out.print(row[i].ordinal());
-			}
-			System.out.println();
-		}
-
+		/*
+		 * InitCellState[][] trueMF = InitMinefield.genMineFieldEnums(10, 10);
+		 * 
+		 * for (InitCellState[] row : trueMF) { for (int i = 0; i < row.length; i++) {
+		 * System.out.print(row[i].ordinal()); } System.out.println(); }
+		 */
+		Scanner scn = new Scanner(System.in);
 		System.out.println();
-		CellField[][] mineSweeperField = InitMinefield.genMineFieldObjects(10, 10);
-
-		mineSweeperField[7][5].setViewState(ViewStatus.UNCOVERED);
-		displayGame(mineSweeperField);
-		mineSweeperField[5][7].setViewState(ViewStatus.UNCOVERED);
-		displayGame(mineSweeperField);
+		System.out.println("How many cells wide would you like your mine?");
+		int minWidth = scn.nextInt();
+		System.out.println("How many cells height would you like your mine?");
+		int minHeight = scn.nextInt();
+		GameInstance runningGame = new GameInstance(minWidth, minHeight);
+		runningGame.displayGame();
+		System.out.println("Which column would you like ot choose?");
+		int col = scn.nextInt();
+		System.out.println("Would you like to choose?");
+		int row = scn.nextInt();
+		runningGame.processInput(col, row, false, 10);
+		runningGame.displayGame();
+		scn.close();
 	}
 
-	public static void displayGame(CellField[][] mineSweeperField) {
-		int k = 1;
-		System.out.println("0 1 2 3 4 5 6 7 8 9 10");
-
-		for (CellField[] row : mineSweeperField) {
-			if (k < 10) {
-				System.out.print(k + " ");
-			} else {
-				System.out.print(k);
-			}
-			for (int i = 0; i < row.length; i++) {
-				System.out.print(" " + row[i].displayCell());
-
-			}
-			k++;
-			System.out.println();
-		}
-	}
 }
